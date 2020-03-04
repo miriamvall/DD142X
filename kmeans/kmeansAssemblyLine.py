60d984fe26e6c39e78bf636647c6acd7e23f63b5
gustav <gustav@gustav.laptop>
2020-03-04 16:37:45 +0100

    More data!

--- kmeans/kmeansAssemblyLine.py
+++ kmeans/kmeansAssemblyLine.py
@@ -11,6 +11,7 @@ inputY = 28                     # Frequencies per file
 chunks = 35                     # 100s/chunks => 10s per chunk
 Xchunkd = floor(193/chunks)     # Amount of time points per chunk
 inputN = 27                     # Amount of files
+Nclusters = 8                   # Number of clusters
 
 # Shape: (files * chunks/file) * (frequencies * N time measurments)
 data = np.zeros((inputN * chunks, inputY * Xchunkd))
@@ -27,9 +28,32 @@ for (dirpath, dirnames, filenames) in walk("../fourierdata/"):
             # i = 0:(inputN-1), j = 0:(chunks-1)
             # Read each chunk, reshape to be vector instead of matrix
             data[i * chunks + j] = arr[:, j * Xchunkd   :   (j + 1) * Xchunkd].reshape((inputY * Xchunkd, ))
+            
+kmeans = KMeans(n_clusters = Nclusters).fit(data)
+clusters = kmeans.cluster_centers_
+distanceMatrix = np.array([np.array([np.linalg.norm(x - y) for x in clusters]) for y in clusters])
 
-kmeans = KMeans(n_clusters = 8).fit(data)
-#print(kmeans.predict(data).reshape((inputN, chunks)))
-#np.savetxt("kMeans.csv", kmeans.cluster_centers_[0] - kmeans.cluster_centers_[1], delimiter = ",")
-np.savetxt("kMeans.csv", kmeans.predict(data).reshape((inputN, chunks)).astype(int), delimiter = ",")
-np.savetxt("centers.csv", kmeans.cluster_centers_, delimiter = ",")
+clusterMeans = np.zeros((Nclusters, Xchunkd * inputY))
+clusterStds = np.zeros((Nclusters, Xchunkd * inputY))
+tmpDir = {}
+for i in range (0, Nclusters):
+    tmpDir[i] = []
+for (x, c) in zip(data, kmeans.predict(data)):
+    for i in range(0, Nclusters):
+        if (c == i):
+            tmpDir[i].append(x)
+for i in range(0, Nclusters):
+    clusterMeans[i] = np.array(tmpDir[i]).mean(axis = 0)
+    clusterStds[i] = np.array(tmpDir[i]).std(axis = 0)
+
+files = [
+    ["clusterMeans", clusterMeans],
+    ["clusterStds", clusterStds],
+    ["clusterMeansStds", clusterMeans.std(axis = 0)],
+    ["kMeans", kmeans.predict(data).reshape((inputN, chunks))],
+    ["centers", kmeans.cluster_centers_],
+    ["clusterDistances", distanceMatrix]
+]
+
+for (fn, xs) in files:
+    np.savetxt("out/" + fn + ".csv", xs, delimiter = ",")
