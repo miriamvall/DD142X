d0d8576b6e197eb6158cfdb3a436b06882231cf4
Gustavrohss <gustav.rohss@gmail.com>
2020-04-01 22:32:43 +0200

    Additional dataset generation.

--- code/model/genData.py
+++ code/model/genData.py
@@ -214,3 +214,129 @@ def generateWaveDataset_type_5(
         "\nSAMPLING FREQUENCY " + str(fs) + "\n"
 
     return values, docString
+
+#####
+#
+#   Not really following documentational standards here.
+#   This is the failed attempt at generating data with the spectogram of the original data as seed.
+#   Failed to interpret the data output, failed to debug the code, moved on to greener pastures
+#
+#####
+
+def generate_ifft_dataset_type_6():
+
+    epochs = 200
+    channels = 25
+    n = 2 ** 13
+    fs = 16000
+
+    # Jury-rig it
+    import matlab_functions as mlf
+    import random
+
+    channelData, _ = mlf.getMatlabValues("../_data/matlabData/NPR-075.b11.mat")
+    seed_values = np.array([
+        # All to same size
+        values[0 : 190 * n] for key, values in channelData.items() if "str_lfp" in key or "gp_lfp" in key
+    # Rows of length n
+    ]).reshape((-1, n))
+
+    samples = np.array(random.sample(list(seed_values), 1000))
+    spectogram = np.fft.fft(samples).sum(axis = 0) / 1000.
+    
+    spectMatrx = np.tile(spectogram, channels).reshape((channels, spectogram.shape[0]))
+    epoch = lambda: np.fft.ifft(
+        (spectMatrx * 
+        np.exp(1j * np.random.uniform(-np.pi, np.pi, size = (channels, n)))) * 
+        np.random.uniform(0.5, 1.5, size = (channels, 1)))
+
+    out = np.concatenate([epoch() for _ in np.arange(0, epochs)], axis = 1)
+    out = np.array([(row - row.mean()) / row.std() for row in out])
+    
+    return out, "\n"
+
+#####
+#
+#   Spectogram-model based data
+#   Uses the spectogram 1/f^alpha
+#   To this adds A * pdf(f) for some pdf ~ N(mu, sigma) and A
+#   Here mu is in the beta-range and sigma is somewhat small
+#
+#   A is chosen per-epoch with high variance (so all the epochs have similar A)
+#   A is then increased or decreased with much lower variance within the epoch (small per-channel variations)
+#
+#####
+
+def generate_ifft_dataset_type_7():
+
+    epochs = 200
+    channels = 25
+    n = 2 ** 13
+    fs = 16000
+
+    pdf = lambda m, s, x: np.exp(-(x - m) ** 2 / (2 * s ** 2)) / np.sqrt(2 * np.pi * s ** 2)
+    spectogram = lambda amp: amp * pdf(21, 5, np.arange(1, n + 1)) + np.arange(1, n+1) ** -1.5
+
+    #####
+    #
+    #   Try this for context:
+    #   plt.plot(np.arange(1, 51), spectogram( 0)[0:50])
+    #   plt.plot(np.arange(1, 51), spectogram( 1)[0:50])
+    #   plt.plot(np.arange(1, 51), spectogram(-1)[0:50])
+    #   plt.show()
+    #
+    #####
+
+    spectMatrix = lambda amp: np.array([
+        spectogram(amp + np.random.uniform(-0.1, 0.1)) for _ in np.arange(0, channels)
+    ])
+    epoch = lambda amp: np.fft.ifft(
+        (spectMatrix(amp) * 
+        np.exp(1j * np.random.uniform(-np.pi, np.pi, size = (channels, n)))))
+
+    out = np.concatenate([epoch(np.random.uniform(2, 4)) for _ in np.arange(0, epochs)], axis = 1)
+    return out, ""
+
+#####
+#
+#   As type 7, but more gaussians, more stochasticity
+#   I don't remember entirely and I am tired at the time of writing
+#
+#####
+
+def type_8(
+    epochs = 200,
+    channels = 25,
+    n = 2 ** 13,
+    alpha = 1.5,
+):
+    rang = np.arange(1, 1 + n)
+    pfd_normal = lambda mu, sigma, x: np.exp( - (x - mu) ** 2 / ( 2 * sigma ** 2) ) \
+        / np.sqrt( 2 * np.pi * sigma ** 2)
+    r_u = lambda x, y: np.random.uniform(x, y)
+
+    spectogram = lambda lo, mid, hi: rang ** -alpha \
+        + lo * pfd_normal(16.5, 3, rang) \
+        + mid* pfd_normal(21,   3, rang) \
+        + hi * pfd_normal(25.5, 3, rang)
+
+    spectEpoch = lambda lo, mid, hi, loInter, midInter, hiInter:\
+        np.array([ spectogram (
+            lo + r_u(-loInter, loInter),
+            mid+ r_u(-midInter, midInter),
+            hi + r_u(-hiInter, hiInter)
+        ) for _ in np.arange(0, channels)])
+
+    epoch = lambda lo, mid, hi, loInter, midInter, hiInter:\
+        np.fft.ifft(
+            spectEpoch(lo, mid, hi, loInter, midInter, hiInter) * 
+            np.exp(1j * np.random.uniform(-np.pi, np.pi, size = (channels, n)))
+        )
+    
+    out = np.concatenate([
+        epoch(
+            r_u(0.8, 2.1), r_u(0.9, 2.2), r_u(0.8, 2.2), 0.1, 0.1, 0.1
+        ) for _ in np.arange(0, epochs)
+    ], axis = 1)
+    
+    return out, ""
